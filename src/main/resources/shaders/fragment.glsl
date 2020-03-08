e382da90e7de9dc42d89be68af69c2791cd8fe0d
Hendrik Müller <hendrik.mueller@devboost.de>
2020-03-08 18:51:18 +0100

    Refactored fragment shader

--- src/main/resources/shaders/fragment.glsl
+++ src/main/resources/shaders/fragment.glsl
@@ -1,27 +1,25 @@
 #version 330
 
-uniform vec4 color;
-
 in  vec2 outTexCoord;
 in vec3 fragPos;
 in vec3 outNormal;
 
-out vec4 outColor;
-
+uniform vec4 color;
 uniform sampler2D textureSampler;
 uniform vec3 lightPos;
 uniform vec3 lightColor;
 
+out vec4 outColor;
+
 vec4 calcDiffuseComponent(vec3 normal, vec3 lightPos, vec3 lightColor, vec3 fragPos) {
-    vec3 norm = normalize(outNormal);
+    vec3 norm = normalize(normal);
     vec3 lightDir = normalize(lightPos - fragPos);
     float diff = max(dot(norm, lightDir), 0.0);
     vec3 diffuse = diff * lightColor;
-
     return vec4(diffuse, 1.0);
 }
 
 void main() {
-    vec4 diffuseComp = calcDiffuseComponent(outNormal, lightPos, fragPos, lightColor);
+    vec4 diffuseComponent = calcDiffuseComponent(outNormal, lightPos, lightColor, fragPos);
     outColor = texture(textureSampler, outTexCoord) * color + diffuseComponent;
 }
